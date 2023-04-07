3625b2d328ba79251ac8ca8a6284fcffc30d27eb
Edward Prokopik <ejprok@gmail.com>
2023-04-07 01:37:22 -0700

    Set up some more config stuff

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.g.mapleader = " "
 vim.g.localleader = "\\"
 
 -- IMPORTS
--- require('vars')      -- Variables
--- require('opts')      -- Options
+require('vars')      -- Variables
+require('opts')      -- Options
 -- require('keys')      -- Keymaps
 -- require('plug')      -- Plugins
